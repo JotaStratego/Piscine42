5122a49baacda28d3fc3aba1e1298081ccd5e3e8
Javier Álvarez Villa <jotastratego@gmail.com>
2024-01-31 09:42:29 +0100

    C00 ex02

---
+++ C00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: javialva <javialva@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/31 09:23:03 by javialva          #+#    #+#             */
+/*   Updated: 2024/01/31 09:23:08 by javialva         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h> 
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	z;
+
+	z = 'z';
+	while (z >= 'a')
+	{
+		write(1, &z, 1);
+		z--;
+	}
+}
+
+/*int	main(void)
+{
+	ft_print_reverse_alphabet();
+	return (0);
+}*/
