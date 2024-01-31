6b07acae13d056fa99ef3c0fcbe9a01ca248cd5d
Javier Álvarez Villa <jotastratego@gmail.com>
2024-01-31 09:42:57 +0100

    C00 ex03

---
+++ C00/ex03/ft_print_numbers.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_numbers.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: javialva <javialva@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/31 09:38:43 by javialva          #+#    #+#             */
+/*   Updated: 2024/01/31 09:38:45 by javialva         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_numbers(void)
+{
+	char	number;
+
+	number = '0';
+	while (number <= '9')
+	{
+		write(1, &number, 1);
+		number++;
+	}
+}
+
+int	main(void)
+{
+	ft_print_numbers();
+	return (0);
+}
