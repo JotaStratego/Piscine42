337d5c13d3b7ad9ed239540a9d64d1b126914fda
Javier Álvarez Villa <jotastratego@gmail.com>
2024-02-08 10:27:04 +0100

    Ex 01

---
+++ C01/ex01/ft_ultimate_ft.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_ft.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: javialva <javialva@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/08 10:00:54 by javialva          #+#    #+#             */
+/*   Updated: 2024/02/08 10:00:56 by javialva         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+void	ft_ultimate_ft(int *********nbr)
+{
+	*********nbr = 42;
+}
+
+/*int	main(void)
+{
+	int	a;
+	int	*pt1;
+	int	**pt2;
+	int	***pt3;
+	int	****pt4;
+	int	*****pt5;
+	int	******pt6;
+	int	*******pt7;
+	int	********pt8;
+	int	*********pt9;
+
+	pt1 = &a;
+	pt2 = &pt1;
+	pt3 = &pt2;
+	pt4 = &pt3;
+	pt5 = &pt4;
+	pt6 = &pt5;
+	pt7 = &pt6;
+	pt8 = &pt7;
+	pt9 = &pt8;
+	a = 50;
+	printf("Antes de ft_ultimate_ft: %d\n", *********pt9);
+	ft_ultimate_ft(pt9);
+	printf("Despues de ft_ultimate_ft: %d", *********pt9);
+	return (0);
+}*/
