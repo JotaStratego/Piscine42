25f62e56a9c9171c4f1b6d0789c7791d8d2a5f27
Javier Álvarez Villa <jotastratego@gmail.com>
2024-02-08 23:21:14 +0100

    Ex03

---
+++ C01/ex03/ft_div_mod.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_div_mod.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: javialva <javialva@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/08 21:02:28 by javialva          #+#    #+#             */
+/*   Updated: 2024/02/08 21:03:28 by javialva         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdio.h>
+
+void	ft_div_mod(int a, int b, int *div, int *mod)
+{
+	*div = a / b;
+	*mod = a % b;
+}
+
+int	main(void)
+{
+	int	a;
+	int	b;
+	int div;
+	int	mod;
+
+	a = 10;
+	b = 5;
+	ft_div_mod(a, b, &div, &mod);
+	printf("%d\n", div);
+	printf("%d", mod);
+	return (0);
+}
