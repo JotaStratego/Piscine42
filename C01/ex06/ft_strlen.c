7ace4f2857df3c0813ceabafe1ecee544430c87a
Javier Álvarez Villa <jotastratego@gmail.com>
2024-02-09 12:22:58 +0100

    Ex 06

---
+++ C01/ex06/ft_strlen.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: javialva <javialva@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/09 12:17:14 by javialva          #+#    #+#             */
+/*   Updated: 2024/02/09 12:17:16 by javialva         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdio.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+int	main(void)
+{
+	char *v = "Hola mundo!";
+	printf("El número de caracteres de la cadena de texto es: %d", ft_strlen(v));
+	return (0);
+}
