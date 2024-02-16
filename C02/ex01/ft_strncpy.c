dcbfd94c68250c473dc6e5477af2d8b0a14c1125
Javier Álvarez Villa <jotastratego@gmail.com>
2024-02-16 09:48:29 +0100

    Ex01

---
+++ C02/ex01/ft_strncpy.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: javialva <javialva@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/16 09:01:19 by javialva          #+#    #+#             */
+/*   Updated: 2024/02/16 09:14:36 by javialva         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
+
+/*int	main(void)
+{
+	char	*cadena;
+	char	dest[20];
+	unsigned int	n;
+
+	cadena = "Hola como estas?";
+	n = 5;
+	ft_strncpy(dest, cadena, n);
+	printf("%s", dest);
+	return (0);
+}*/
