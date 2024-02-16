ff5f632d7479ede8b4ab496adb0d5ddaa88e0f2f
Javier Alvarez Villa <jotastratego@gmail.com>
2024-02-16 17:18:01 +0100

    Ex06

---
+++ C02/ex06/ft_str_is_printable.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_printable.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: javialva <javialva@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/16 17:10:43 by javialva          #+#    #+#             */
+/*   Updated: 2024/02/16 17:17:37 by javialva         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_str_is_printable(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (str[i] == '\0')
+		return (1);
+	while (str[i] != '\0')
+	{
+		if (!(str[i] >= 32 && str[i] <= 126))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+/* Caracteres imprimibles van del 32 al 126. El 32 es space pero tambien
+es imprimible.*/
