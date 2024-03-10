dade2f358bcb306cd304a61707113547ebd092d6
Javier Alvarez Villa <jotastratego@gmail.com>
2024-03-10 18:01:25 +0100

    Ex02 C03

---
+++ C03/ex02/ft_strcat.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcat.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: javialva <javialva@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/10 17:52:18 by javialva          #+#    #+#             */
+/*   Updated: 2024/03/10 18:00:26 by javialva         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_strlen(char *dest)
+{
+	int	i;
+
+	i = 0;
+	while (dest[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strcat(char *dest, char *src)
+{
+	int	i;
+	int	len;
+
+	i = 0;
+	len = ft_strlen(dest);
+	while (src[i] != '\0')
+	{
+		dest[len + i] = src[i];
+		i++;
+	}
+	dest[len + i] = '\0';
+	return (dest);
+}
